a451af787f7b1288c51f659cd75a98803d192b99
muhamad waqas <93639960+Muhammad-AdapptLimited@users.noreply.github.com>
2022-10-21 14:25:20 +0500

    Update index.js

--- polyfill/index.js
+++ polyfill/index.js
@@ -6,6 +6,9 @@ import Event from './Event'
 import FileReader from './FileReader'
 import Fetch from './Fetch'
 
+import {NativeModules} from 'react-native';
+const RNFetchBlob = NativeModules.RNFetchBlob
+
 export default {
   Blob, File, XMLHttpRequest, ProgressEvent, Event, FileReader, Fetch
 }
